1695ee7d517ffaaf43432b7a8c504c42cac09e1c
adityavyadav <adityavyadav5969@gmail.com>
2026-06-13 12:51:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+  let error = document.getElementById("error");
+
+  if (name === "" || email === "" || message === "") {
+    error.style.color = "red";
+    error.textContent = "Please fill all fields!";
+  } else {
+    error.style.color = "green";
+    error.textContent = "Message sent successfully!";
+    
+    document.getElementById("contactForm").reset();
+  }
+});
